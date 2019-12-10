83198f8ef740f7bd0d42e71dd67487338b1f40bc
kassadie barney <kassadie.b161@stu.nebo.edu>
2019-12-10 10:30:22 -0700

    4.1 part 7

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed;
+    private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        focalPoint
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+    }
+}
